4298f4cbf3981c2b6a779d68a4df30edc8b9251d
LalithAbhiram <lalithabhiram2@gmail.com>
2018-07-28 16:44:03 +0530

    created index.html

---
+++ sample.js
@@ -0,0 +1,13 @@
+function validate() 
+{
+var username =document.getElementById("username");
+var password=documnet.getElementById("password");
+
+if(username=="admin"&&password=="admin")
+{
+alert("login Succesful");
+}
+else{
+alert("unsuccessful");
+    }
+}
